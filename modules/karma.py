044528681f344c338079a1380991f2343f3f91ad
floordiv <pavloromanenko25@gmail.com>
2020-08-30 12:25:52 +0300

    removed python-formatting, now using sql parameters

--- modules/karma.py
+++ modules/karma.py
@@ -7,13 +7,14 @@ import syst.mworker as mworker
 dbm.open_db('karma', 'CREATE TABLE IF NOT EXISTS chats (chat string, username string, karma float)')
 
 
-@mworker.handler(lambda msg: msg.content[0] in ('+', '-'))
+@mworker.handler(lambda msg: msg.content[0] in ('+', '-') and msg.replied)
 def karma_action(wrapper, msg):
-    dbm.execute('karma', f'SELECT * FROM chats WHERE chat = "{msg.chat}" AND username = "{msg.author.userid}"')
+    dbm.execute('karma', 'SELECT * FROM chats WHERE chat = ? AND username = ?',
+                parameters=(msg.chat, msg.author.userid))
     initiator = dbm.fetchone('karma')
 
     if initiator is None:
-        dbm.execute('karma', f"INSERT INTO chats VALUES ('{msg.chat}', '{msg.author.userid}', 1.0)", autocommit=True)
+        dbm.execute('karma', f"INSERT INTO chats VALUES (?, ?, 1.0)", parameters=(msg.chat, msg.author.userid), autocommit=True)
         chat, initiator_user_id, initiator_karma = msg.chat, msg.author.userid, 1.0
     elif initiator[2] <= .0:
         return wrapper.replymsg(msg, f'Your karma is not enough ({initiator[2]})')
@@ -25,18 +26,19 @@ def karma_action(wrapper, msg):
     if msg.content[0] == '-':
         add_karma = -add_karma
 
-    dbm.execute('karma', f'SELECT * FROM chats WHERE chat = "{msg.chat}" AND username = "{msg.replied.author.userid}"')
+    dbm.execute('karma', f'SELECT * FROM chats WHERE chat = ? AND username = ?', parameters=(msg.chat, msg.replied.author.userid))
     user_exists = dbm.fetchone('karma')
 
     if user_exists:
-        dbm.execute('karma', f'UPDATE chats SET karma = karma + {add_karma} WHERE chat = "{msg.chat}" AND username = "{msg.replied.author.userid}"')
+        dbm.execute('karma', f'UPDATE chats SET karma = karma + ? WHERE chat = ? AND username = ?',
+                    parameters=(add_karma, msg.chat, msg.replied.author.userid))
     else:
-        dbm.execute('karma', f'INSERT INTO chats VALUES ("{msg.chat}", "{msg.replied.author.userid}", {1 + add_karma})')
+        dbm.execute('karma', f'INSERT INTO chats VALUES (?, ?, ?)', parameters=(msg.chat, msg.replied.author.userid, 1 + add_karma))
 
     dbm.commit('karma')
 
     # get new user's karma
-    dbm.execute('karma', f'SELECT * FROM chats WHERE chat = "{msg.chat}" AND username = "{msg.replied.author.userid}"')
+    dbm.execute('karma', f'SELECT * FROM chats WHERE chat = ? AND username = ?', parameters=(msg.chat, msg.replied.author.userid))
     _, replied_userid, new_replied_user_karma = dbm.fetchone('karma')
     new_replied_user_karma = round(new_replied_user_karma, 2)
 
